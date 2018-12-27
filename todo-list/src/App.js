119a306b8c5f7de2a601ae3c01832a69884fcc65
Your Name <you@example.com>
2018-12-26 19:58:42 -0800

    Adding an Input and button.

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -4,13 +4,17 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-
+            todos: []
         };
     }
 
     render() {
         return (
-
+            <div>
+                <input placeholder="Enter a Todo" />
+                <button>Add</button>
+                { this.state.todos.length === 0 ? "No Todos" : "You have Todos" }
+            </div>
         );
     }
 }
